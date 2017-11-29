347ed4613dd6bd89125869e2bd17b5503bfd451b
Mike Solomon <mike@mikesolomon.org>
2017-11-29 18:56:42 +0100

    fixes namespace clash

--- src/routes/data.js
+++ src/routes/data.js
@@ -24,12 +24,12 @@ export default (makePost = false) => {
     try {
       client.connect();
       await makeTable(client);
-      await new Promise((resolve, reject) => client.query('SELECT data FROM data_to_serve ORDER BY id ASC LIMIT $1 OFFSET $2;', [req.params.n, req.params.o], (err, res) => {
+      await new Promise((resolve, reject) => client.query('SELECT data FROM data_to_serve ORDER BY id ASC LIMIT $1 OFFSET $2;', [req.params.n, req.params.o], (err, sqlRes) => {
         if (err) {
           reject(err);
           return;
         }
-        res.json(res.rows.map(row => JSON.parse(row.data)));
+        res.json(sqlRes.rows.map(row => JSON.parse(row.data)));
         resolve(true);
       }));
     } catch (e) {
@@ -47,7 +47,7 @@ export default (makePost = false) => {
       try {
         client.connect();
         await makeTable(client);
-        await new Promise((resolve, reject) => client.query(`INSERT INTO data_to_serve (data) VALUES ${req.body.map((_, i) => `($${i+1}) `).join(',')};`, req.body.map(row => JSON.stringify(row)), (err, res) => {
+        await new Promise((resolve, reject) => client.query(`INSERT INTO data_to_serve (data) VALUES ${req.body.map((_, i) => `($${i+1}) `).join(',')};`, req.body.map(row => JSON.stringify(row)), (err, sqlRes) => {
           if (err) {
             reject(err);
             end;

51d72c4a873d2e55feeb3fa88429454f926568dd
Mike Solomon <mike@mikesolomon.org>
2017-11-29 22:10:12 +0100

    removes spurious log

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
 app.use('/', data(false));
-console.log("MEESHKAN_KEY from the process", process.env.MEESHKAN_KEY);
 app.use(`/${process.env.MEESHKAN_KEY || "you-should-never-use-this-for-any-serious-deployment"}`, data(true));
 
 export default app;

55563652ce94d7e498f0fec696a9a92bbdc20ff0
Mike Solomon <mike@mikesolomon.org>
2017-11-30 07:35:19 +0100

    Adds more robust api

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser())
-app.use('/', data(false));
-app.use(`/${process.env.MEESHKAN_KEY || "you-should-never-use-this-for-any-serious-deployment"}`, data(true));
+app.use('/o', data(false));
+app.use(`/i/${process.env.MEESHKAN_KEY || "you-should-never-use-this-for-any-serious-deployment"}`, data(true));
 
 export default app;
 
--- src/routes/data.js
+++ src/routes/data.js
@@ -5,7 +5,7 @@ import {
   Client
 } from 'pg';
 
-const makeTable = client => new Promise((resolve, reject) => client.query('CREATE TABLE IF NOT EXISTS data_to_serve (id SERIAL, data TEXT NOT NULL);', (err, res) => {
+const makeTable = client => new Promise((resolve, reject) => client.query('CREATE TABLE IF NOT EXISTS data_to_serve (id SERIAL, data TEXT NOT NULL, tag VARACHAR(64) NOT NULL);', (err, res) => {
   if (err) {
     reject(err);
     return;
@@ -13,53 +13,96 @@ const makeTable = client => new Promise((resolve, reject) => client.query('CREAT
   resolve(true);
 }));
 
-export default (makePost = false) => {
-  const router = Router();
-  router.get('/', async (req, res, next) => {
-    const client = new Client({
-      connectionString: process.env.DATABASE_URL,
-      ssl: true,
-    });
-    try {
-      client.connect();
-      await makeTable(client);
-      await new Promise((resolve, reject) => client.query('SELECT data FROM data_to_serve ORDER BY id ASC LIMIT $1 OFFSET $2;', [req.query.n, req.query.o], (err, sqlRes) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        res.json(sqlRes.rows.map(row => JSON.parse(row.data)));
-        resolve(true);
-      }));
-    } catch (e) {
-      console.error(e);
-      res.status(500).send('Server error');
-    }
-    client.end();
+const makeGet = async (req, res, next) => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   });
-  if (makePost) {
-    router.post('/', async (req, res, next) => {
-      const client = new Client({
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-      });
-      try {
-        client.connect();
-        await makeTable(client);
-        await new Promise((resolve, reject) => client.query(`INSERT INTO data_to_serve (data) VALUES ${req.body.map((_, i) => `($${i+1}) `).join(',')};`, req.body.map(row => JSON.stringify(row)), (err, sqlRes) => {
-          if (err) {
-            reject(err);
-            end;
-          }
-          res.send(200);
-          resolve(true);
-        }));
-      } catch (e) {
-        console.error(e);
-        res.status(500).send('Server error');
+  try {
+    client.connect();
+    const {
+      tag,
+      id
+    } = req.params || {};
+    await makeTable(client);
+    await new Promise((resolve, reject) => client.query(`SELECT id, data, tag FROM data_to_serve ${(tag && id) ? 'WHERE tag = $3 AND id = $4' : tag ? 'WHERE tag = $3' : ''} ORDER BY id ASC LIMIT $1 OFFSET $2;`, [req.query.n, req.query.o, ...[tag, id].filter(_=>_)], (err, sqlRes) => {
+      if (err) {
+        reject(err);
+        return;
       }
-      client.end();
-    });
+      res.json(sqlRes.rows.map(row => req.query.tag ? { tag: row.tag, id: row.id, data: JSON.parse(row.data)} : JSON.parse(row.data)));
+      resolve(true);
+    }));
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Server error');
+  }
+  client.end();
+}
+
+const makeDelete = async (req, res, next) => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+  try {
+    client.connect();
+    const {
+      tag,
+      id
+    } = req.params || {};
+    await makeTable(client);
+    await new Promise((resolve, reject) => client.query(`DELETE FROM data_to_serve ${(tag && id) ? 'WHERE tag = $1 AND id = $2' : tag ? 'WHERE tag = $1' : ''};`, [tag, id].filter(_=>_), (err, sqlRes) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(true);
+    }));
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Server error');
+  }
+  client.end();
+}
+
+const makePost = async (req, res, next) => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  });
+  try {
+    client.connect();
+    const {
+      tag
+    } = req.params || {};
+    await makeTable(client);
+    await new Promise((resolve, reject) => client.query(`INSERT INTO data_to_serve (data${tag ? ', tag' : ''}) VALUES ${req.body.map((_, i) => `($${i+1}${tag ? ', $'+(req.body.length + 1) : ''}) `).join(',')};`, [...req.body.map(row => JSON.stringify(row)), ...(tag ? [tag] : [])], (err, sqlRes) => {
+      if (err) {
+        reject(err);
+        end;
+      }
+      res.send(200);
+      resolve(true);
+    }));
+  } catch (e) {
+    console.error(e);
+    res.status(500).send('Server error');
+  }
+  client.end();
+}
+
+export default (makeMutable = false) => {
+  const router = Router();
+  router.get('/', makeGet);
+  router.get('/:tag', makeGet);
+  router.get('/:tag/:id', makeGet);
+  if (makeMutable) {
+    router.post('/', makePost);
+    router.post('/:tag', makePost);
+    router.delete('/', makeDelete);
+    router.delete('/:tag', makeDelete);
+    router.delete('/:tag/:id', makeDelete);
   }
   return router;
 }
